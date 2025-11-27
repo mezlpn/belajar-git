a98a922a7971ebfec081f7c2e6806bb7e0db4e7a
mezlpn <135789591+mezlpn@users.noreply.github.com>
2025-11-27 11:47:11 +0700

    Add greeting message with name and relationship
    hehe

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
     cout << "This is a simple C++ program." << endl;
+    cout << "Namaku Zulfan, Pacarku 3" << endl;
     return 0;
 }
